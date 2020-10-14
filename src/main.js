b3d046714361ae4d99df1697edfd7234a9322b8d
František Gič <mcfreddie777@gmail.com>
2020-10-14 19:40:41 +0200

    PW-9 Integrate Font Awesome icons with project

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+// Font Awesome Icons
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+
+// Root styles
 import '@/assets/scss/app.scss';
 
 Vue.config.productionTip = false;
