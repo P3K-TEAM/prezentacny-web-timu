d3ccdaf41b16a4a8c654e395e94f223c95aa9cb1
Denisa Mensatorisová <32281312+DeMensa123@users.noreply.github.com>
2020-10-20 19:46:45 +0200

    PW-12 create contact page

---
+++ src/components/ContactItem.vue
@@ -0,0 +1,26 @@
+<template>
+    
+    <div class="py-4 text-left px-4">
+        <div class="border-b-2 border-primary mx-auto">
+            <i :class="icon"></i>
+            <p>{{ msg }}</p>
+        </div>
+    </div>  
+    
+</template>
+
+
+
+
+
+<script>
+export default {
+	props: {
+        'msg': String,
+        'icon': String
+    }
+};
+</script>
+
+
+
---
+++ src/views/Contact.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="bg-gray-200">
+        <div class="container mt-40 flex mx-auto text-left">
+            <div class="w-1/2 my-5">
+                <div >
+                    <strong class="text-xl">Napíšte nám</strong>
+                    <p>V prípade akýchkoľvek otázok nás neváhajte kontaktovať na jednom z uvedených linkov.</p>
+                </div>
+            </div>
+            <div class="w-1/2 mx-24">
+                <div>
+                <ContactItem
+                    v-for="(item, index) in items"
+                    :key="index"
+                    :msg="item.msg"
+                    :icon="item.icon"
+                >
+                </ContactItem>
+                </div>  
+            </div>
+        </div>
+  
+
+    </div>
+
+    
+</template>
+
+<script>
+
+import ContactItem from '@/components/ContactItem.vue';
+
+export default {
+    components: {
+		ContactItem
+    },
+    data: function() {
+		return {
+            items :[
+                {
+                    msg:'tim10fiit@googlegroups.com',
+                    icon: 'far fa-envelope'
+                },
+                {
+                    msg:'Fakulta informatiky a informačných technologií' +
+                    'Slovenská Technická Univerzita v Bratislave' +
+                    'Iľkovičova 2' +
+                    '842 16 Bratislava',
+                    icon:'fas fa-map-marker-alt'
+                }
+            ]
+		};
+	}
+}
+</script>
