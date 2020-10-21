c9edbaf7b628af1c95bdeb05becffe854a60ccca
František Gič <mcfreddie777@gmail.com>
2020-10-21 13:35:26 +0200

    PW-12 Reformat code with prettier

--- src/components/ContactItem.vue
+++ src/components/ContactItem.vue
@@ -1,26 +1,17 @@
 <template>
-    
-    <div class="py-4 text-left px-4">
-        <div class="border-b-2 border-primary mx-auto">
-            <i :class="icon"></i>
-            <p>{{ msg }}</p>
-        </div>
-    </div>  
-    
+	<div class="py-4 text-left px-4">
+		<div class="border-b-2 border-primary mx-auto">
+			<i :class="icon"></i>
+			<p>{{ text }}</p>
+		</div>
+	</div>
 </template>
-
-
-
-
 
 <script>
 export default {
 	props: {
-        'msg': String,
-        'icon': String
-    }
+		text: String,
+		icon: String
+	}
 };
 </script>
-
-
-
