6efef6ef358dc9ade042d47c37339bbce93b1c64
František Gič <mcfreddie777@gmail.com>
2020-10-23 15:31:17 +0200

    PW-13 Restyle Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,52 @@
 <template>
-	<div class="bg-gray-100 ">
-		<!-- header title + logo -->
-		<div class="container flex mx-auto py-12"> 
-			<div class="row">
-				<div class="col-md-8">
-					<h1 class="font-bold" style="font-size: 500%;">AntiPlag</h1>
-					<div class="text-primary font-bold" style="font-size: 200%;">
-						<p>
-							NEDAJTE SA OJE***!
-						</p>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<img src="edited/home.png" style="max-width: 65%;">
-				</div>
+	<div class="bg-primary">
+		<div class="flex bg-gray-100 py-48 skewed-bottom-wrapper">
+			<div class="flex flex-col w-1/2 justify-center items-center">
+				<h1 class="font-bold text-6xl">AntiPlag</h1>
+				<span class="text-primary text-xl font-bold uppercase">
+					Nedajte si svoj nápad uje***!
+				</span>
 			</div>
+			<div class="flex w-1/2 justify-center"></div>
 		</div>
-		<!-- Main description -->
-		<div class="flex justify-center mw-100 bg-primary py-32 ">
-			<div class="w-3/5 text-white" style="">
-				<h2 class="text-xl font-bold" style="font-size: 200%">O Projekte</h2>
-				<p style="margin: 0.7em 0em">
-					S rozvojom IT technológií problematika plagiátorstva nadobúda nové rozmery. 
-					Voľne prístupné texty umožňujú ľuďom rýchlo, ľahko a bez vynaloženej námahy získať potrebné informácie. 
-					Preto sme spojili naše sily, aby sme vytvorili projekt, ktorý prinesie spravodlivosť a odhalí nepoctivo vytvorené práce.
-					Našim cieľom je vytvorenie nástroja na odhaľovanie plagiátorstva v písanom texte a aj zdrojových kódoch.
-				</p>
-				<h2 class="text-xl font-bold" style="font-size: 200%">O tíme</h2>
-				<p style="margin: 0.7em 0em">
-					P3K Team sa skladá zo 7 tímových hráčov s kolektívnym duchom, ktorému nechýba chuť pracovať. 
-					Každý z nás sme síce iný, ale kombináciou našich schopností a vedomostí tvoríme silný tím, ktorý od začiatku až do konca udržiava pracovné nasadenie v priateľskej atmosfére. 
-					Našou silnou stránkou je zodpovedný prístup. 
-					Máme v tíme ľudí kreatívnych a spoľahlivých, ľudí s organizačnými schopnosťami, súťaživých, priateľských, či tých, čo vedia inšpirovať ostatných. 
-				</p>
-			</div>
+
+		<div class="container mx-auto text-white py-32 text-justify">
+			<h2 class="text-3xl font-bold ">
+				O Projekte
+			</h2>
+			<p class="mt-2 mb-10">
+				S rozvojom IT technológií problematika plagiátorstva nadobúda
+				nové rozmery. Voľne prístupné texty umožňujú ľuďom rýchlo, ľahko
+				a bez vynaloženej námahy získať potrebné informácie. Preto sme
+				spojili naše sily, aby sme vytvorili projekt, ktorý prinesie
+				spravodlivosť a odhalí nepoctivo vytvorené práce. Našim cieľom
+				je vytvorenie nástroja na odhaľovanie plagiátorstva v písanom
+				texte a aj zdrojových kódoch.
+			</p>
+
+			<h2 class="text-3xl font-bold">
+				O tíme
+			</h2>
+			<p class="mt-2">
+				P3K Team sa skladá zo 7 tímových hráčov s kolektívnym duchom,
+				ktorému nechýba chuť pracovať. Každý z nás sme síce iný, ale
+				kombináciou našich schopností a vedomostí tvoríme silný tím,
+				ktorý od začiatku až do konca udržiava pracovné nasadenie v
+				priateľskej atmosfére. Našou silnou stránkou je zodpovedný
+				prístup. Máme v tíme ľudí kreatívnych a spoľahlivých, ľudí s
+				organizačnými schopnosťami, súťaživých, priateľských, či tých,
+				čo vedia inšpirovať ostatných.
+			</p>
 		</div>
 	</div>
 </template>
 
 <script>
-export default {
-	components: {},
-	data: function() {
-		return {};
-	}
-};
+export default {};
 </script>
+
+<style lang="scss">
+.skewed-bottom-wrapper {
+	clip-path: polygon(0 0, 100% 0%, 100% 100%, 0% 88%);
+}
+</style>
