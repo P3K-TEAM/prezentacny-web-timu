e3abccc6c41ee0fb2cec4ac716d9fd0858af1806
Kristian Toldy <kristian.toldy@gmail.com>
2020-10-23 12:42:33 +0200

    PW-13 Create "O projekte" view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,41 @@
 <template>
-	<div></div>
+	<div class="bg-gray-100 ">
+		<!-- header title + logo -->
+		<div class="container flex mx-auto py-12"> 
+			<div class="row">
+				<div class="col-md-8">
+					<h1 class="font-bold" style="font-size: 500%;">AntiPlag</h1>
+					<div class="text-primary font-bold" style="font-size: 200%;">
+						<p>
+							NEDAJTE SA OJE***!
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<img src="edited/home.png" style="max-width: 65%;">
+				</div>
+			</div>
+		</div>
+		<!-- Main description -->
+		<div class="flex justify-center mw-100 bg-primary py-32 ">
+			<div class="w-3/5 text-white" style="">
+				<h2 class="text-xl font-bold" style="font-size: 200%">O Projekte</h2>
+				<p style="margin: 0.7em 0em">
+					S rozvojom IT technológií problematika plagiátorstva nadobúda nové rozmery. 
+					Voľne prístupné texty umožňujú ľuďom rýchlo, ľahko a bez vynaloženej námahy získať potrebné informácie. 
+					Preto sme spojili naše sily, aby sme vytvorili projekt, ktorý prinesie spravodlivosť a odhalí nepoctivo vytvorené práce.
+					Našim cieľom je vytvorenie nástroja na odhaľovanie plagiátorstva v písanom texte a aj zdrojových kódoch.
+				</p>
+				<h2 class="text-xl font-bold" style="font-size: 200%">O tíme</h2>
+				<p style="margin: 0.7em 0em">
+					P3K Team sa skladá zo 7 tímových hráčov s kolektívnym duchom, ktorému nechýba chuť pracovať. 
+					Každý z nás sme síce iný, ale kombináciou našich schopností a vedomostí tvoríme silný tím, ktorý od začiatku až do konca udržiava pracovné nasadenie v priateľskej atmosfére. 
+					Našou silnou stránkou je zodpovedný prístup. 
+					Máme v tíme ľudí kreatívnych a spoľahlivých, ľudí s organizačnými schopnosťami, súťaživých, priateľských, či tých, čo vedia inšpirovať ostatných. 
+				</p>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
