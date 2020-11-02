ad8f7939541c8126cdfc8fbda4a3e2fa186718e4
H3MII <rusnak.anton3@gmail.com>
2020-11-02 21:10:54 +0100

    PW-20 Add unit test for Person

---
+++ tests/unit/Person.spec.js
@@ -0,0 +1,23 @@
+import { mount } from '@vue/test-utils';
+import Person from '../../src/components/Person/Person';
+import { expect } from 'chai';
+
+describe('Person Component', function() {
+	it('render info for person component', function() {
+		//render component
+		const wrapper = mount(Person, {
+			propsData: {
+				name: 'Ing. Juraj Petrík',
+				description: 'Vedúci tímu, Product Owner',
+				image: 'edited/juraj-petrik.png'
+			}
+		});
+
+		expect(wrapper.text()).to.contain(
+			'Ing. Juraj Petrík',
+			'Vedúci tímu, Product Owner',
+			'edited/juraj-petrik.png'
+		);
+
+	});
+});
