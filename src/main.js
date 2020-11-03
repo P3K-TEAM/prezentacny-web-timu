516bf9960d2c5b7db8df4e16ee5e0407ed40e199
Denisa Mensatorisová <32281312+DeMensa123@users.noreply.github.com>
2020-11-03 13:53:49 +0100

    PW-22 Add dynamic page title

--- src/main.js
+++ src/main.js
@@ -11,6 +11,13 @@ import '@/assets/scss/app.scss';
 
 Vue.config.productionTip = false;
 
+// Dynamic page title (argument to is the item from router/index.js routes array)
+router.afterEach(to => {
+	Vue.nextTick(() => {
+		document.title = `${to.name ? `${to.name} | ` : ''}P3K Team`;
+	});
+});
+
 new Vue({
 	router,
 	render: h => h(App)
